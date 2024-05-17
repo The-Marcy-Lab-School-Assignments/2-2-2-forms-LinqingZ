8125274cbd1e3cac7bedd4bf11721cb16127a751
LinqingZ <linqing201@gmail.com>
2024-05-17 02:16:20 -0400

    solved half of questions

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
  *
  */
 
-const handleSubmit = () => {
+const handleSubmit = (event) => {
+    event.preventDefault()
 };
+
+const form = e.target;
+console.log("The actual input element (or node list if it's a radio group)", form.username)
+console.log('The final computed value', form.username.value)
+
+const formData = new FormData(e.target);
+const { username } = Object.fromEntries(formData);
+
+console.log('The final computed value', username)
