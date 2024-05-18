53b10adac0ed676c8a6f105c7731a6c655a72ed7
LinqingZ <linqing201@gmail.com>
2024-05-18 02:02:56 -0400

    solved all problems

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 /** GENERAL FEEDBACK:
  *
  */
-
-const handleSubmit = (event) => {
-    event.preventDefault()
-};
-
-const form = e.target;
-console.log("The actual input element (or node list if it's a radio group)", form.username)
-console.log('The final computed value', form.username.value)
-
-const formData = new FormData(e.target);
-const { username } = Object.fromEntries(formData);
-
-console.log('The final computed value', username)
+const form = document.getElementById("new-user-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //   console.log(e.target)
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+//   console.log(formData);
+  data.didLikeAssignment = form.didLikeAssignment.checked ? "Yes" : "No";
+//   console.log(data);
+  document.querySelector("#results-username").textContent = data.username;
+  document.querySelector("#results-coding-level").textContent = data.codingLevel;
+  document.querySelector("#results-location").textContent = data.location;
+  document.querySelector("#results-did-like-assignment").textContent = data.didLikeAssignment;
+  document.getElementById("new-user-form").reset();
+});
